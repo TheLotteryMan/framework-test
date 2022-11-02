de96c05fe9510f1e2c241249e8ce292a86dd363a
Lance Griggs <lanceagriggs@gmail.com>
2022-11-02 11:20:00 -0400

    Barebones setup with POM.xml builds and dependencies. Added .gitignore folders Added BrowserUtils, ConfigurationReader and Driver classes to utilities Added Hooks Class to step_definitions Added FailedTestRunner, updated Hooks Added CukesRunner class...

---
+++ src/test/java/com/trycloud/runner/CukesRunner.java
@@ -0,0 +1,21 @@
+package com.trycloud.runner;
+
+import io.cucumber.junit.Cucumber;
+import io.cucumber.junit.CucumberOptions;
+import org.junit.runner.RunWith;
+
+@RunWith(Cucumber.class)
+@CucumberOptions(
+        plugin = {
+                "html:target/cucumber-report.html",
+                "rerun:target/rerun.txt",
+                "me.jvt.cucumber.report.PrettyReports:target/cucumber"
+        },
+        features = "src/test/resources/features", // from content root
+        glue = "com/trycloud/step_definitions", // from source root
+        dryRun = false,
+        tags = ""
+)
+public class CukesRunner {
+
+}
